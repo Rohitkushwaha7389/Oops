9a130d4dfc30c27c53bb5a1e54a5ef2d02cca4a4
Rohit <rohitkushwaha7389@gmail.com>
2023-01-30 23:25:58 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    int a;
+    String name;
+    public static void main(String[] args) {
+        int num=9;
+        Test obj=new Test();
+        Test obj1=new Test();
+        System.out.println(obj.a);
+        System.out.println(obj.name);
+        System.out.println(obj1.a);
+        System.out.println(obj1.name);
+    }
+    
+}
