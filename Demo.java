190f10b624a4aac4c3156a01eaf22b33f7e91d61
Rohit <rohitkushwaha7389@gmail.com>
2023-01-30 23:25:43 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,11 @@
+class Demo{
+    int a;
+    String name;
+    public static void main(String[] args) {
+        // variable declare
+        //create a object
+        int num=9;
+        Demo obj=new Demo();
+        System.out.println(obj.a);
+    }
+}
